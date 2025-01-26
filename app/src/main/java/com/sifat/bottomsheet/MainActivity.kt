324702e6b186081783032e4aebbbf263d0c135fa
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-01-26 19:01:08 +0600

    Initial Commit

--- app/src/main/java/com/sifat/bottomsheet/MainActivity.kt
+++ app/src/main/java/com/sifat/bottomsheet/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.sifat.bottomsheet
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+    }
+    fun showBottomSheet(view:View){
+        val dialog = BottomSheetDialog(this)
+        val view = layoutInflater.inflate(R.layout.bottom_sheet, null)
 
+        dialog.setCancelable(true)
+        dialog.setContentView(view)
+        dialog.show()
     }
 }
